9cd7aa65d640626ab43a1bcab86a210a956abe6b
ak-matsu <sandbox.matsu@gmail.com>
2021-06-29 01:42:52 +0900

    追加条件の処理を行うコードをコントローラーに追記

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,14 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all  # 全商品の情報を取得
     set_product_column       # privateメソッド内で定義
+    set_category_column
+
   end
 
   def search
     @results = @p.result.includes(:category)  # 検索条件にマッチした商品の情報を取得
+    @product_size = Product.select("size").distinct
+    @product_status = Product.select("status").distinct
   end
 
   private
@@ -19,6 +23,10 @@ class ProductsController < ApplicationController
 
   def set_product_column
     @product_name = Product.select("name").distinct  # 重複なくnameカラムのデータを取り出す
+  end
+
+  def set_category_column
+    @category_name = Category.select("name").distinct
   end
 
 end
